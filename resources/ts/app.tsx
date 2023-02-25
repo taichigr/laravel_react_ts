8670cc5e8ad7e9e0d07138e4cbef77fd22390605
taichi <taichi.01gr14@gmail.com>
2023-02-25 18:27:20 +0900

    edit app.tsx

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,9 +1,20 @@
 import "../css/app.css";
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import { Router } from "./router/Router";
+import ProvideAuth from "./providers/Auth/AuthProvider";
+
+// import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 
 const App = () => {
-    return <h1 className="text-red-400 text-4xl"></h1>;
+    return (
+        <>
+          <ProvideAuth>
+              <Router />
+          </ProvideAuth>
+        </>
+    );
 };
 const container = document.getElementById('app') as HTMLInputElement;
 const root = createRoot(container);
