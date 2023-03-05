79fe9d651673ece0f174228016d3842103d68f11
taichi <taichi.01gr14@gmail.com>
2023-03-05 17:54:46 +0900

    generate 404page

---
+++ resources/ts/components/pages/Page404.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+
+export const Page404 = () => {
+    return (
+        <>
+            <div>page404</div>
+        </>
+    )
+}
+
+
+
