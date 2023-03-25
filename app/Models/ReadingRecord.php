af2c293b0e74938828c0d671ac5a89d762fe6f50
taichi <taichi.01gr14@gmail.com>
2023-03-25 21:29:35 +0900

    make model

--- app/Models/ReadingRecord.php
+++ app/Models/ReadingRecord.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class ReadingRecord extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'read_date',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
