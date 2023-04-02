a37676d6b40649863a9213b6a7761e328b4cb382
taichi <taichi.01gr14@gmail.com>
2023-04-03 08:18:20 +0900

    リファクタリング

--- resources/ts/features/search/components/BookCard.tsx
+++ resources/ts/features/search/components/BookCard.tsx
@@ -1,7 +1,6 @@
-import React, { memo, useState, VFC } from "react";
-import { useHistory } from "react-router-dom";
-import { Volume } from "../types";
+import React, { VFC } from "react";
 import { Link } from "react-router-dom";
+import { Volume } from "../types";
 import {
     getId,
     presentAuthor,
@@ -13,32 +12,30 @@ interface Props {
     item: Volume;
 }
 
-export const BookCard: VFC<Props> = memo((props) => {
-    const history = useHistory();
-    const { item } = props;
+export const BookCard: VFC<Props> = ({ item }) => {
+    const { volumeInfo } = item;
+    const { imageLinks, title, authors, publishedDate } = volumeInfo;
 
     return (
-        <>
-            <div className="flex bg-white border border-gray-200 rounded-lg shadow h-56 items-center">
-                <div className="w-32">
-                    <img
-                        loading="lazy"
-                        className="object-cover w-full rounded"
-                        src={item.volumeInfo.imageLinks?.smallThumbnail}
-                        alt=""
-                    />
-                </div>
-
-                <div className="p-2 leading-normal w-3/5 h-full">
-                    <h3 className="bold text-xl p-1 text-teal-500 hover:text-blue-500">
-                        <Link to={`/book/${getId(item)}`}>
-                            {trimText(item.volumeInfo.title, 50)}
-                        </Link>
-                    </h3>
-                    <p>{presentAuthor(item.volumeInfo.authors)}</p>
-                    <p>{presentText(item.volumeInfo.publishedDate)}</p>
-                </div>
+        <div className="flex bg-white border border-gray-200 rounded-lg shadow h-56 items-center">
+            <div className="w-32">
+                <img
+                    loading="lazy"
+                    className="object-cover w-full rounded"
+                    src={imageLinks?.smallThumbnail}
+                    alt=""
+                />
             </div>
-        </>
+
+            <div className="p-2 leading-normal w-3/5 h-full">
+                <h3 className="bold text-xl p-1 text-teal-500 hover:text-blue-500">
+                    <Link to={`/book/${getId(item)}`}>
+                        {trimText(title, 50)}
+                    </Link>
+                </h3>
+                <p>{presentAuthor(authors)}</p>
+                <p>{presentText(publishedDate)}</p>
+            </div>
+        </div>
     );
-});
+};
