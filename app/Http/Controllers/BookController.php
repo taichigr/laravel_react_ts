e1ec0655d1f37b57564730173f0e2f8b530b1689
taichi <taichi.01gr14@gmail.com>
2023-04-03 07:58:25 +0900

    不要箇所削除

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,48 +14,6 @@ use Illuminate\Http\Response;
 class BookController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index(): Response
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request): Response
-    {
-        //
-        dd('this is bookcontroller transaction. store action method');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id): Response
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id): Response
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id): Response
-    {
-        //
-    }
-
-
-    /**
      * updateReadingStatus
      *
      * @param UpdateReadingStatusRequest $request
