ec8e65e43617347de98d014f0c51d589acea80d9
taichi <taichi.01gr14@gmail.com>
2023-04-03 07:57:50 +0900

    book status 定数追加

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -18,6 +18,26 @@ class Book extends Model
         'image_url',
     ];
 
+    // ステータス定数
+    const STATUS_TO_READ = 'to_read';
+    const STATUS_READING = 'reading';
+    const STATUS_FINISHED = 'finished';
+    const STATUS_UNREAD = 'unread';
+
+
+    // ステータスのラベル
+    public static $statusLabels = [
+        self::STATUS_TO_READ => '読みたい',
+        self::STATUS_READING => '読んでいる途中',
+        self::STATUS_FINISHED => '読み終わった',
+        self::STATUS_UNREAD => '積読',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function readingRecords()
     {
         return $this->hasMany(ReadingRecord::class);
