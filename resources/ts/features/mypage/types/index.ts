2416be7de4707af6e1746587e7483545869d3432
taichi <taichi.01gr14@gmail.com>
2023-04-06 22:12:13 +0900

    edit mypage セレクトボックスで本リスト出しわけ

---
+++ resources/ts/features/mypage/types/index.ts
@@ -0,0 +1,22 @@
+export interface ReadingRecord {
+    key: string;
+    id: number;
+    book_id: number;
+    user_id: number;
+    read_date: string;
+    status: string;
+    created_at: string;
+    updated_at: string;
+    book: Book;
+}
+
+export interface Book {
+    id: number;
+    title: string;
+    author: string;
+    publisher: string;
+    google_book_id: string;
+    image_url: string;
+    created_at: string;
+    updated_at: string;
+}
