875a7ab458d6f9e85da68f38ec689215281b0d06
taichi <taichi.01gr14@gmail.com>
2023-04-07 08:20:17 +0900

    本詳細に未選択を追加

--- resources/ts/features/bookDetail/components/SelectBookStatus.tsx
+++ resources/ts/features/bookDetail/components/SelectBookStatus.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useCallback, useEffect } from "react";
+import React, { memo, useCallback, useEffect, useState } from "react";
 import { useAuth } from "../../../lib/Auth";
 import { useUpdateBookStatus } from "../hooks/useUpdateBookStatus";
 
@@ -24,15 +24,15 @@ export const SelectBookStatus = memo(
         });
 
         const handleAuthenticatedChange = useCallback(
-          async (event: React.ChangeEvent<HTMLSelectElement>) => {
-              if (!auth.user) {
-                  alert("ログインが必要です。");
-              } else {
-                  handleChange(event);
-              }
-          },
-          [auth.user, handleChange]
-      );
+            async (event: React.ChangeEvent<HTMLSelectElement>) => {
+                if (!auth.user) {
+                    alert("ログインが必要です。");
+                } else {
+                    handleChange(event);
+                }
+            },
+            [auth.user, handleChange]
+        );
         return (
             <div>
                 <select
@@ -40,6 +40,11 @@ export const SelectBookStatus = memo(
                     onChange={handleAuthenticatedChange}
                     className="w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
                 >
+                    {selectedValue || defaultStatus || (
+                        <>
+                            <option>未選択</option>
+                        </>
+                    )}
                     <option value="to_read">読みたい</option>
                     <option value="reading">読んでいる途中</option>
                     <option value="finished">読み終わった</option>
