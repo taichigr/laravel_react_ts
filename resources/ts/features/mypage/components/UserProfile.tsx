8582c614646c2f1f0894b0cf863fb677868c5bd6
taichi <taichi.01gr14@gmail.com>
2023-04-10 07:50:36 +0900

    スタイル変更

--- resources/ts/features/mypage/components/UserProfile.tsx
+++ resources/ts/features/mypage/components/UserProfile.tsx
@@ -1,21 +1,24 @@
 import React, { VFC } from "react";
 import { PersonIcon } from "@radix-ui/react-icons";
+import { Link } from "react-router-dom";
 
 type Props = {
-  name: string;
+    name: string;
 };
 
 export const UserProfile: VFC<Props> = ({ name }) => {
-  return (
-    <div className="p-2 bg-white border border-gray-200 rounded-lg shadow h-36">
-      <div className="flex items-center">
-        <div className="bg-gray-300 p-2 rounded-full text-white">
-          <PersonIcon />
+    return (
+        <div className="p-2 bg-white border border-gray-200 rounded-lg shadow h-36">
+            <Link to="/profile">
+                <div className="flex items-center mb-4">
+                    <div className="bg-gray-300 p-4 rounded-full text-white">
+                        <PersonIcon />
+                    </div>
+                    <div className="ml-4">
+                        <h1 className="text-xl font-bold">{name}</h1>
+                    </div>
+                </div>
+            </Link>
         </div>
-        <div className="ml-2">
-          <p>{name}</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
