abe0aadedb776a7f4191cf485da7ca8b7d461963
taichi <taichi.01gr14@gmail.com>
2023-04-20 08:19:28 +0900

    CheckRecordAction

--- app/UseCases/Book/CheckRecordAction.php
+++ app/UseCases/Book/CheckRecordAction.php
@@ -4,16 +4,17 @@ namespace App\UseCases\Book;
 
 use App\Models\Book;
 use App\Models\ReadingRecord;
-use App\Models\User;
+use App\Http\Requests\Book\CheckRecordRequest;
 
 class CheckRecordAction
 {
-    public function __invoke(User $user, string $googleBooksId): array
+    public function __invoke(CheckRecordRequest $request)
     {
-        // Find book by google_books_id
+        $user = $request->user();
+        $googleBooksId = $request->google_books_id;
+
         $book = Book::where('google_books_id', $googleBooksId)->first();
 
-        // If book is not found, return early with no record found
         if (!$book) {
             return [
                 'exists' => false,
@@ -22,7 +23,6 @@ class CheckRecordAction
         }
 
         $readingRecordQuery = ReadingRecord::where(['book_id' => $book->id, 'user_id' => $user->id]);
-
         $readingRecordExists = $readingRecordQuery->exists();
         $readingRecord = $readingRecordQuery->first();
 
