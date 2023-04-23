db547ea97fd060bc42e9f09924481ed3f7026c09
taichi <taichi.01gr14@gmail.com>
2023-04-23 11:56:39 +0900

    FindBookByGoogleBooksIdAction

--- app/UseCases/Book/FindBookByGoogleBooksIdAction.php
+++ app/UseCases/Book/FindBookByGoogleBooksIdAction.php
@@ -4,6 +4,7 @@ namespace App\UseCases\Book;
 
 use App\Models\Book;
 use App\Http\Requests\Book\FindBookByGoogleBooksIdRequest;
+use App\Models\Review;
 
 class FindBookByGoogleBooksIdAction
 {
@@ -11,7 +12,12 @@ class FindBookByGoogleBooksIdAction
     {
         $googleBooksId = $request->input('google_books_id');
         $book = Book::where('google_books_id', $googleBooksId)->first();
+        
+        $review = Review::where('book_id', $book->id)->first();
 
-        return $book;
+        return [
+            'book' => $book,
+            'review' => $review,
+        ];
     }
 }
